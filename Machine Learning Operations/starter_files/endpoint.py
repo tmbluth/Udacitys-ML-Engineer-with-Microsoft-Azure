e4763d4ff6c9e546fe0480ad8a26430a4943fdb4
Taylor Bluth <tmbluth@gmail.com>
2021-07-09 13:32:29 -0600

    working code to be submitted

--- Machine Learning Operations/starter_files/endpoint.py
+++ Machine Learning Operations/starter_files/endpoint.py
@@ -3,9 +3,9 @@ import json
 
 # URL for the web service, should be similar to:
 # 'http://8530a665-66f3-49c8-a953-b82a2d312917.eastus.azurecontainer.io/score'
-scoring_uri = ''
+scoring_uri = 'http://45ec5d48-63f5-40e7-a555-ace608f1f463.centralus.azurecontainer.io/score'
 # If the service is authenticated, set the key or token
-key = ''
+key = 'bo2tIm6JVYUQdt7y61mpG5AojRTxXxbc'
 
 # Two sets of data to score, so we get two results back
 data = {"data":
@@ -56,6 +56,11 @@ data = {"data":
           },
       ]
     }
+
+
+
+print(data)
+
 # Convert to JSON string
 input_data = json.dumps(data)
 with open("data.json", "w") as _f:
@@ -68,6 +73,7 @@ headers['Authorization'] = f'Bearer {key}'
 
 # Make the request and display the response
 resp = requests.post(scoring_uri, input_data, headers=headers)
+print(resp)
 print(resp.json())
 
 
